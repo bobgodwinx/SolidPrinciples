8ca7f4aa1c2d666cfc945c2da27709d1858bf23e
Obi Bob Godwin <bobgodwinx@gmail.com>
2018-03-23 15:26:02 +0100

    SOLID: Added Switch class  that conforms to `Switchable`

--- SingleResponsibility.playground/Contents.swift
+++ SingleResponsibility.playground/Contents.swift
@@ -9,7 +9,23 @@ protocol Switchable {
     func on()
     func off()
 }
-// Executes a function
+/// Executes a function
 protocol Executable {
     func execute()
+}
+/// Conforms to the `Switchable`
+class Switch: Switchable {
+    private var state: Bool = false
+
+    func on() {
+        state = true
+    }
+
+    func off() {
+        state = false
+    }
+
+    var description: String {
+        return state ? "Switched On" : "Switched Off"
+    }
 }
