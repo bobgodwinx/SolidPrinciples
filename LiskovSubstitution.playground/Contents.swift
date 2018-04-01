f98fb2de960153676a5da6b514719f1e3980278b
bobgodwinx <bobgodwinx@gmail.com>
2018-04-01 11:54:58 +0200

    SOLID: Introduced the `Vehicle` base class for transportation

--- LiskovSubstitution.playground/Contents.swift
+++ LiskovSubstitution.playground/Contents.swift
@@ -4,3 +4,19 @@ import Foundation
 import PlaygroundSupport
 
 //MARK: Liskov Substitution
+
+class Vehicle {
+    let name: String
+    let speed: Double
+    let wheels: Int
+
+    init(name: String, speed: Double, wheels: Int) {
+        self.name = name
+        self.speed = speed
+        self.wheels = wheels
+    }
+
+    func canfly() -> Bool {
+        return false
+    }
+}
