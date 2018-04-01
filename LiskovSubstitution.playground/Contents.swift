7ccc086fb573b26d42e418b4cc04c8f5eea2b0cb
bobgodwinx <bobgodwinx@gmail.com>
2018-04-01 16:52:47 +0200

    SOLID: Added `speedDescription` fuction and it's usage

--- LiskovSubstitution.playground/Contents.swift
+++ LiskovSubstitution.playground/Contents.swift
@@ -28,3 +28,20 @@ class Airplane: Vehicle {
 }
 
 class Car: Vehicle { }
+
+class Handler {
+    static func speedDescription(for vehicle: Vehicle) -> String {
+        if vehicle.canfly() {
+            return "It flys at \(vehicle.speed) knot "
+        } else {
+            return "Runs at \(vehicle.speed) kph"
+        }
+    }
+}
+
+let car = Car(name: "BMW", speed: 180.00, wheels: 4)
+let plane = Airplane(name: "KLM", speed: 34.00, wheels: 3)
+///Based on Liskov Handler should always be able to
+///Process speedDescription when given any type of Vehicle
+let carSpeedDescription = Handler.speedDescription(for: car)
+let planeSpeedDescription = Handler.speedDescription(for: plane)
