c6c6ed56bcd7daf0f8283d1d6f869981ecd863fe
Obi Bob Godwin <bobgodwinx@gmail.com>
2018-04-05 16:40:04 +0200

    SOLID: Added the `State` enum

--- InterfaceSegregation.playground/Contents.swift
+++ InterfaceSegregation.playground/Contents.swift
@@ -17,3 +17,16 @@ enum Language {
     case mandarin, english, deutsche, italian, latin, spanish, french
 }
 
+enum State {
+    case italy(continent: Continent)
+    case gemany(continent: Continent)
+    case southAfrica(continent: Continent)
+    case nigeria(continent: Continent)
+    case china(continent: Continent)
+    case brazil(continent: Continent)
+    case USA(continent: Continent)
+    case scotland(continent: Continent)
+    case england(continent: Continent)
+    case wales(continent: Continent)
+    case northernIreland(continent: Continent)
+}
