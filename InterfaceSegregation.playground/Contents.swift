b116d476550ab7ab79b9725f31dbf2c7de67c57e
Obi Bob Godwin <bobgodwinx@gmail.com>
2018-04-05 16:39:15 +0200

    SOLID: Added Union, Language and updated Continent enums

--- InterfaceSegregation.playground/Contents.swift
+++ InterfaceSegregation.playground/Contents.swift
@@ -6,5 +6,14 @@ import PlaygroundSupport
 //MARK: Interface Segregation
 
 enum Continent: String {
-    case africa, europe, asia
+    case africa, europe, asia, america
 }
+
+enum Union {
+    case EU, AU, CAU, NAU, UNASUR
+}
+
+enum Language {
+    case mandarin, english, deutsche, italian, latin, spanish, french
+}
+
