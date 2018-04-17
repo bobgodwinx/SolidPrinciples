0b8585c1697c1a2028bfc36ddb80ddb2f5b91ee6
Obi Bob Godwin <bobgodwinx@gmail.com>
2018-04-17 18:14:23 +0200

    SOLID: Added the `DefaultsServiceType` which defines the biz intent

--- DependencyInversion.playground/Contents.swift
+++ DependencyInversion.playground/Contents.swift
@@ -4,3 +4,25 @@ import Foundation
 import PlaygroundSupport
 
 //MARK: Dependency Inversion"
+
+typealias DefaultsKey = String
+/// A type providing DAO services for `UserDefaults` persistence.
+protocol DefaultsServiceType {
+    /// Read & decode a value for a given key from standard `UserDefaults`.
+    ///
+    /// - Parameter forKey: Defaults key to be read.
+    /// - Returns: Decoded value if possible. `nil` if not present in standard defaults or if decoding threw an error.
+    func read<T: Decodable>(forKey: DefaultsKey) -> T?
+    /// Encode & write a value to a give key in standard `UserDefaults`.
+    ///
+    /// If encoding the given value failed, existing value -if any- will be erased.
+    ///
+    /// - Parameters:
+    ///   - value: Value to encode and write to defaults.
+    ///   - forKey: Defaults key to write to
+    func write<T: Encodable>(_ value: T, forKey: DefaultsKey)
+    /// Erase a value for a given key -if present- from standard `UserDefaults`.
+    ///
+    /// - Parameter key: Defaults key to erase.
+    func erase(_ key: DefaultsKey)
+}
